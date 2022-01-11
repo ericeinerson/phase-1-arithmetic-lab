a38d3141d03549d99fd445797507be52b15fe49e
Eric Einerson <eaeiner.gmail.co>
2022-01-10 20:25:40 -0700

    Finished Lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1=2;
+let num2=31;
+let multiply = num1*num2;
+let random = Math.floor(Math.random()+1);
+let num3 = 64
+let num4 = 5
+let mod = (num3%num4)
+let max = Math.max(20,14,15,1)
 // Write your code here
